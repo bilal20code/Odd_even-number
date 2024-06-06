450fa8f670358cf04aa061788309ed28a2bf5d8e
Muhammad Bilal Ahmad <111138242+bilal20code@users.noreply.github.com>
2024-06-06 15:04:46 +0500

    update Code
    update Code

--- main.dart
+++ main.dart
@@ -1,12 +1,15 @@
 void main(){
   List<int> num = [9,83,9,97,545,315,16,3,6,4,5,7];
+  List evennumber=[];
+  List oddnumber = [];
    
   for (var abc in num) {
     if (abc % 2 == 0) {
-    print("This $abc is even number");
+      evennumber.add(abc);
   }else{
-    print("This $abc is  Odd number");
+      oddnumber.add(abc);  }
   }
-  }
-  
+  print("This is even numbers : $evennumber");
+  print("This is odd numbers : $oddnumber");
+
 }
